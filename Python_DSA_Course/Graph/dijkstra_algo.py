34d690b998a93a7635a1f7475e0955e72704ba4f
SanskarLoganDev <devlogan70@gmail.com>
2025-12-07 09:58:34 -0500

    Dijkstra's Algorithm

---
+++ Python_DSA_Course/Graph/dijkstra_algo.py
@@ -0,0 +1,26 @@
+# Dijkstra's Algorithm to find the shortest path from a source vertex to all other vertices in a weighted graph
+
+import heapq
+class Solution:
+    # Returns shortest distances from src to all other vertices
+    def dijkstra(self, V, edges, src):
+        adj = [[] for _ in range(V)] # converting edge list to adjacency list
+        result = [float("inf")]*V # to store shortest distance from src to each vertex
+        for u, v, dist in edges: # edges contain (u, v, weight)
+            adj[u].append((v, dist))
+            adj[v].append((u, dist))
+        min_heap = [] # min-heap to get the vertex with the smallest distance
+        result[src] = 0 # distance from src to itself is 0
+        heapq.heappush(min_heap, (0, src))
+        
+        while min_heap:
+            dist, u = heapq.heappop(min_heap) # get vertex with smallest distance
+            if dist > result[u]: # if this distance is not up to date, skip it (This if statement is not strictly necessary but optimizes performance)
+                continue
+            for v, weight in adj[u]: # iterate through all adjacent vertices of u
+                if dist+weight<result[v]:
+                    heapq.heappush(min_heap, (dist+weight, v)) # push updated distance to heap
+                    result[v] = dist+weight # update shortest distance to vertex v
+                    
+        return result # return the list of shortest distances from src to all vertices
+        
