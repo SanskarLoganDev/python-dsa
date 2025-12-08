f17430d24ac69546f82ef81a6388ce4f186b5a63
SanskarLoganDev <devlogan70@gmail.com>
2025-12-08 08:28:21 -0500

    Dijkstra's Algorithm added time complexity comments

--- Python_DSA_Course/Graph/dijkstra_algo.py
+++ Python_DSA_Course/Graph/dijkstra_algo.py
@@ -13,14 +13,18 @@ class Solution:
         result[src] = 0 # distance from src to itself is 0
         heapq.heappush(min_heap, (0, src))
         
-        while min_heap:
-            dist, u = heapq.heappop(min_heap) # get vertex with smallest distance
+        while min_heap: # V vertices (O(V))
+            dist, u = heapq.heappop(min_heap) # get vertex with smallest distance. pop is O(log V)
             if dist > result[u]: # if this distance is not up to date, skip it (This if statement is not strictly necessary but optimizes performance)
                 continue
-            for v, weight in adj[u]: # iterate through all adjacent vertices of u
+            for v, weight in adj[u]: # iterate through all adjacent vertices of u. operating on all edges therefore O(E)
                 if dist+weight<result[v]:
-                    heapq.heappush(min_heap, (dist+weight, v)) # push updated distance to heap
+                    heapq.heappush(min_heap, (dist+weight, v)) # push updated distance to heap. O(log V)
                     result[v] = dist+weight # update shortest distance to vertex v
                     
         return result # return the list of shortest distances from src to all vertices
         
+# Total Time Complexity: O(V*(log V + E*log(V))) = O((1 + E) Vlog V) ~ EVlog(V)
+# Space Complexity: O(V + E) for adjacency list + O(V) for result array + O(V) for heap = O(V + E)
+
+# Worst case, V vertex are conected with V-1 vertices = V*(V-1) = V^2 edges
